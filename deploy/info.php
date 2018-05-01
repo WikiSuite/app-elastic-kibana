3bfc535b1988b2a47a7ab2b98c02a437d89f7223
pcbaldwin <peter@egloo.ca>
2018-05-01 15:15:34 -0400

    Buildsys retry

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'elastic_kibana';
-$app['version'] = '1.2.5';
+$app['version'] = '1.2.7';
 $app['release'] = '1';
 $app['vendor'] = 'WikiSuite';
 $app['packager'] = 'eGloo';
--- packaging/app-elastic-kibana.spec
+++ packaging/app-elastic-kibana.spec
@@ -1,7 +1,7 @@
 
 Name: app-elastic-kibana
 Epoch: 1
-Version: 1.2.5
+Version: 1.2.7
 Release: 1%{dist}
 Summary: Kibana
 License: GPLv3
